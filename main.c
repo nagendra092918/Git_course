65d083533482ec7ef5f58950e3b2a65b0d9afde8
Marella nagendra babu <nagendra092918@gmail.com>
2025-11-16 01:15:11 +0530

    new

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 not added
 added discription in main.c file
+=======
+added discription in main.c file
+files not present
+>>>>>>> ebe831eee00bc28e9244cb8ba476c710bae15de0
 =======
 added discription in main.c file
 files not present
